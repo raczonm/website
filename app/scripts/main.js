2979bbfc56e651c3c2759a25b1763cd2c2250ab6
michal.raczon <michal.raczon@grandparade.co.uk>
2016-02-07 10:07:24 +0100

    [website] panels nav up and running

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,25 +7,29 @@ function prepareApp() {
             proportions = windowWidth/windowHeight;
 
         if (proportions > 1.50) {
-            console.log("yuyu");
             var fontSize = windowHeight * 0.135;
             $('body, .column').css('font-size', fontSize+'px');
         }
-        console.log(proportions);
     }
 
     app.setPanelSnap = function(){
-      console.log("A")
         var options = {
-           $menu: false,
-           menuSelector: 'a',
-           panelSelector: 'header, section, footer',
+           $menu: $('.scroll-nav'),
+           menuSelector: '.scroll-nav-top, scroll-nav-bottom',
+           navigation: {
+                buttons: {
+                    $nextButton: $('.scroll-nav-up'),
+                    $prevButton: $('.scroll-nav-down')
+                }
+           },
+           panelSelector: '.panel',
            namespace: '.panelSnap',
            onSnapStart: function(){},
            onSnapFinish: function(){},
            directionThreshold: 30,
            slideSpeed: 300
         }
+
         $('body').panelSnap(options);
     }
 
