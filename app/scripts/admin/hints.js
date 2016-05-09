27d21490eed6d403809e66461d71cd20fbb94d8e
michal.raczon <michal.raczon@grandparade.co.uk>
2016-05-09 12:59:28 +0200

    [admin] finished v0.5

--- app/scripts/admin/hints.js
+++ app/scripts/admin/hints.js
@@ -13,16 +13,22 @@
         room.ui = {
             $section: $('.hints.' + name),
             $list: $('.hints.' + name + ' .hints-list'),
+            $adhoc: $('.hints.' + name + ' .hints-header-adhoc'),
             $adhocInput: $('.hints.' + name + ' .hints-header-adhoc-text'),
             $adhocButton: $('.hints.' + name + ' .hints-header-adhoc-button'),
             $switch: $('.hints.' + name + ' .hints-header-switch'),
-            $timer: $('.hints.' + name + ' .hints-header-timer')
+            $timer: $('.hints.' + name + ' .hints-header-timer'),
+            $languageSwitch: $('.hints.' + name + ' .hints-footer-language-button')
         }
         room.setActiveHint = function(id){
             room.data.update({'active': id});
+            room.ui.$adhoc.removeClass('active');
         }
         room.setAdhoc = function() {
-            room.data.update({'active': 0, 'adhoc': room.ui.$adhocInput.val()});
+            if(room.ui.$section.hasClass('running')){
+                room.data.update({'active': 0, 'adhoc': room.ui.$adhocInput.val()});
+                room.ui.$adhoc.addClass('active');
+            }
         }
         room.toggleSwitch = function(e){
             if ($(e.currentTarget).hasClass('on')){
@@ -31,12 +37,30 @@
                 room.startGame();
             }
         }
+        room.switchLanguage = function(){
+            var newLang = room.ui.$languageSwitch.hasClass('eng') ? 'pl' : 'eng';
+            room.ui.$languageSwitch.toggleClass('eng');
+            room.data.update({'language': newLang});
+        }
         room.startGame = function(){
             room.ui.$switch.addClass('on');
             room.ui.$section.addClass('running');
             room.data.on('value', room.printHints);
-            room.data.update({'started': true, 'timer': Date.now()});
+            room.data.update({'started': true, 'timer': Date.now(), 'active': false, 'adhoc': ''});
             room.startTimer();
+        },
+        room.restartGame = function(data){
+            room.ui.$switch.addClass('on');
+            room.ui.$section.addClass('running');
+            room.ui.$adhocInput.val(data.adhoc);
+            if(data.active === 0){
+                room.ui.$adhoc.addClass('active');
+            }
+            room.data.on('value', room.printHints);
+            room.startTimer();
+            if (data.language === 'eng') {
+                room.ui.$languageSwitch.addClass('eng');
+            }
         },
         room.stopGame = function(){
             clearInterval(room.timeCounter);
@@ -44,22 +68,25 @@
             room.ui.$section.removeClass('running');
             room.data.off('value');
             room.ui.$list.html('');
-            room.data.update({'active': false, 'adhoc': '', 'started': false, 'timer': false});
+            room.timeGone = false;
+            room.ui.$adhoc.removeClass('active');
+            room.data.update({'active': false, 'adhoc': '', 'started': false, 'timer': false, 'language': 'pl'});
+            room.ui.$languageSwitch.removeClass('eng');
         },
-        room.startTimer = function(){
-            var count = 3600;
+        room.startTimer = function(time){
+            var count = 3600 - (room.timeGone || 0);
             room.timeCounter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
             function timer() {
-              count = count - 1;
-              if (count == -1) {
-                  room.stopGame();
-                  return;
-              }
-              var seconds = count % 60;
-              var minutes = Math.floor(count / 60);
-              minutes %= 60;
-              room.ui.$timer.html(minutes + ":" + seconds); // watch for spelling
+                count = count - 1;
+                if (count <= 0) {
+                    room.stopGame();
+                    return;
+                }
+                var seconds = count % 60;
+                var minutes = Math.floor(count / 60);
+                minutes %= 60;
+                room.ui.$timer.html(minutes + ":" + seconds); // watch for spelling
             }
         }
         room.printHints = function(snapshot){
@@ -69,16 +96,30 @@
             for (var i = 0; i < data.list.length; i++) {
                 var item = data.list[i],
                     active = item.id === data.active ? 'active' : '',
-                    content = item.type === 'text' ? '<p class="hints-list-item-text">' + item.text + '</p>' : '<img class="hints-list-item-image" src="/' + item.img + '" />',
+                    isPl = data.language === 'pl' ? true : false,
+                    textPrimary = isPl ? item.text : item.text_eng,
+                    textSmall = isPl ? item.text_eng : item.text,
+                    content = item.type === 'text' ? '<p class="hints-list-item-text">' + textPrimary + '<span class="hints-list-item-text-small">' + textSmall+ '</span></p>' : '<img class="hints-list-item-image" src="/' + item.img + '" />',
                     button = '<a class="hints-list-item-button button" data-id="' + item.id + '">Set active</a>',
                     el = $('<li data-id="' + item.id + '" class="hints-list-item ' + active + '"> <h4 class="hints-list-item-title">' + item.name + '</h4>' + content + button + '</li>');
-                    el.on('click', function(){room.setActiveHint($(this).data('id'))});
+                el.on('click', function(){room.setActiveHint($(this).data('id'))});
                 list.append(el);
             }
         }
 
-
+        room.data.once('value', function(snapshot){
+            var data = snapshot.val();
+            if(data.started) {
+                room.timeGone = Math.round((Date.now() - data.timer)/1000);
+                if(room.timeGone < 3600){
+                    room.restartGame(data);
+                } else {
+                    room.stopGame();
+                }
+            }
+        });
         room.ui.$switch.on('click', room.toggleSwitch);
+        room.ui.$languageSwitch.on('click', room.switchLanguage);
         room.ui.$adhocButton.on('click', room.setAdhoc);
 
         return room;
