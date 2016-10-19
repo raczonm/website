37e02b5a46525588301b9142c93bde03997b1842
Michał Raczoń <itsatrapescape@gmail.com>
2016-10-19 19:49:32 +0200

    [website] new admin styles part1

--- app/scripts/admin/hints.js
+++ app/scripts/admin/hints.js
@@ -18,18 +18,21 @@
             $adhocButton: $('.hints.' + name + ' .hints-header-adhoc-button'),
             $switch: $('.hints.' + name + ' .hints-header-switch'),
             $timer: $('.hints.' + name + ' .hints-header-timer'),
-            $languageSwitch: $('.hints.' + name + ' .hints-footer-language-button')
+            $languageSwitch: $('.hints.' + name + ' .language-switch')
         };
+
         room.setActiveHint = function(id){
             room.data.update({'active': id});
             room.ui.$adhoc.removeClass('active');
         };
+
         room.setAdhoc = function() {
             if(room.ui.$section.hasClass('running')){
                 room.data.update({'active': 0, 'adhoc': room.ui.$adhocInput.val()});
                 room.ui.$adhoc.addClass('active');
             }
         };
+
         room.toggleSwitch = function(e){
             if ($(e.currentTarget).hasClass('on')){
                 room.stopGame();
@@ -37,11 +40,14 @@
                 room.startGame();
             }
         };
+
         room.switchLanguage = function(){
             var newLang = room.ui.$languageSwitch.hasClass('eng') ? 'pl' : 'eng';
+            room.ui.$languageSwitch.text(newLang);
             room.ui.$languageSwitch.toggleClass('eng');
             room.data.update({'language': newLang});
         };
+
         room.startGame = function(){
             room.ui.$switch.addClass('on');
             room.ui.$section.addClass('running');
@@ -50,19 +56,24 @@
             room.data.update({'started': true, 'timer': Date.now(), 'active': false, 'adhoc': ''});
             room.startTimer();
         };
+
         room.restartGame = function(data){
             room.ui.$switch.addClass('on');
             room.ui.$section.addClass('running');
             room.ui.$adhocInput.val(data.adhoc);
+
             if(data.active === 0){
                 room.ui.$adhoc.addClass('active');
             }
+
             room.data.on('value', room.printHints);
             room.startTimer();
+
             if (data.language === 'eng') {
                 room.ui.$languageSwitch.addClass('eng');
             }
         };
+
         room.stopGame = function(){
             clearInterval(room.timeCounter);
             room.ui.$switch.removeClass('on');
@@ -74,20 +85,22 @@
             room.data.update({'active': false, 'adhoc': '', 'started': false, 'timer': false, 'language': 'pl'});
             room.ui.$languageSwitch.removeClass('eng');
         };
+
         room.startTimer = function(){
             var count = 3600 - (room.timeGone || 0);
+
             function timer() {
                 count = count - 1;
                 if (count <= 0) {
-                  console.log('?');
                     clearInterval(room.timeCounter);
                     room.timeCounter = setInterval(timerUp, 1000); //1000 will  run it every 1 second
                 }
-                var seconds = count % 60;
-                var minutes = Math.floor(count / 60);
+                var seconds = count % 60,
+                    minutes = Math.floor(count / 60);
                 minutes %= 60;
                 room.ui.$timer.html(minutes + ':' + seconds); // watch for spelling
             }
+
             function timerUp() {
                 count = count + 1;
                 var seconds = count % 60;
@@ -107,10 +120,8 @@
                     active = item.id === data.active ? 'active' : '',
                     isPl = data.language === 'pl' ? true : false,
                     textPrimary = isPl ? item.text : item.text_eng,
-                    textSmall = isPl ? item.text_eng : item.text,
-                    content = item.type === 'text' ? '<p class="hints-list-item-text">' + textPrimary + '<span class="hints-list-item-text-small">' + textSmall + '</span></p>' : '<img class="hints-list-item-image" src="/' + item.img + '" />',
-                    button = '<a class="hints-list-item-button button" data-id="' + item.id + '">Set active</a>',
-                    el = $('<li data-id="' + item.id + '" class="hints-list-item ' + active + '"> <h4 class="hints-list-item-title">' + item.name + '</h4>' + content + button + '</li>');
+                    content = item.type === 'text' ? '<p class="hints-list-item-text">' + textPrimary + '</p>' : '<img class="hints-list-item-image" src="/' + item.img + '" />',
+                    el = $('<li data-level="' + item.level + '" data-id="' + item.id + '" class="hints-list-item ' + active + '"> <h4 class="hints-list-item-title">' + item.name + '</h4>' + content + '</li>');
 
                 // UNCOMMENT THIS WHEN READY
                 list.append(el);
@@ -131,6 +142,7 @@
                 }
             }
         });
+
         room.ui.$switch.on('click', room.toggleSwitch);
         room.ui.$languageSwitch.on('click', room.switchLanguage);
         room.ui.$adhocButton.on('click', room.setAdhoc);
